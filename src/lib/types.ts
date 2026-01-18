903b49058e11b4546dbe318382df3a8fcdd8b428
Sakar KC <kcsakar99@gmail.com>
2026-01-18 18:55:48 +0545

    feat(ui): add meter component

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -17,7 +17,7 @@ export type LiquidContents = {
 	concentration: number;
 };
 
-export type EquipmentType = 'elbow'|'beaker' | 'flask' | 'burner' | 'burette'| 'storagetank'|'pipe'
+export type EquipmentType = 'meter'|'elbow'|'beaker' | 'flask' | 'burner' | 'burette'| 'storagetank'|'pipe'
   | "reactor"
   | "compressor"
   | "condenser"
